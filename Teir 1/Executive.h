b32f213dfd8702e76095efb7eb450e3e20fe2553
David0-KU <89867762+David0-KU@users.noreply.github.com>
2022-10-16 20:49:12 -0500

    Create Executive.h
    exec.h of original file broken down

---
+++ Teir 1/Executive.h
@@ -0,0 +1,18 @@
+#ifndef EXECUTIVE_H
+#define EXECUTIVE_H
+#include <string.h>
+
+using namespace std;
+
+class Executive{
+	private:
+
+	public:
+	    string path;
+        string home;
+        string current="";
+		string echo(string input);
+		string pwd();
+        int run();
+};
+#endif
