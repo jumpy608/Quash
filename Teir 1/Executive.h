545e47262bc5b341972d501f4646eb57fac5ecbe
David0-KU <89867762+David0-KU@users.noreply.github.com>
2022-10-16 21:56:39 -0500

    Update Executive.h
    updated to match current .cpp file

--- Teir 1/Executive.h
+++ Teir 1/Executive.h
@@ -1,18 +1,18 @@
 #ifndef EXECUTIVE_H
 #define EXECUTIVE_H
 #include <string.h>
-
 using namespace std;
 
 class Executive{
 	private:
 
 	public:
-	    string path;
+	string path;
         string home;
         string current="";
-		string echo(string input);
-		string pwd();
+	string echo(string input);
+	string pwd();
+	bool cmdInputHandler(string input);
         int run();
 };
 #endif
